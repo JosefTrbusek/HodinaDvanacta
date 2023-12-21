167f24c1a43890d011bac90716ac5c088354813c
trbus <trbus@NOTEBOOK-PEPA>
2023-12-21 23:12:13 +0100

    Add project files.

---
+++ HodinaDvanacta/Account.cs
@@ -0,0 +1,37 @@
+﻿namespace HodinaDvanacta
+{
+    internal class Account
+    {
+        public string AccountNumber { get; private set; }
+        public string OwnerName { get; private set; }
+        public decimal Balance { get; private set; }
+
+        public Account(string accountNumber, string ownerName)
+        {
+            AccountNumber = accountNumber;
+            OwnerName = ownerName;
+            Balance = 0;
+        }
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Castka nemuze být záporná", nameof(amount));
+            }
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Castka nemuze být záporná", nameof(amount));
+            }
+            if (amount > Balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Castka prekracuje zustatek");
+            }            
+            Balance -= amount;            
+        }
+    }
+}
---
+++ HodinaDvanacta/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HodinaDvanacta;
+
+try
+{
+    Account ucet = new Account("1/0100", "Radovan Krejčíř");
+    Console.WriteLine("Na účtu zbývá: " + ucet.Balance);
+    //ucet.Withdraw(1_000_000);
+    //Console.WriteLine("Na účtu zbývá: " + ucet.Balance);
+    ucet.Deposit(1_000_000);
+    Console.WriteLine("Na účtu zbývá: " + ucet.Balance);
+    ucet.Withdraw(100_000);
+    Console.WriteLine("Na účtu zbývá: " + ucet.Balance);
+    ucet.Withdraw(-100_000);
+    Console.WriteLine("Na účtu zbývá: " + ucet.Balance);
+}
+catch(ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine($"{ex.Message}");
+}
+catch(ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadLine();
+
